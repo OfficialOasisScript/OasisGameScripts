dcd377f33d96ff4b956b2233ac5be95897ed3e6a
Oasis <143208824+OfficialOasisScript@users.noreply.github.com>
2023-08-25 12:58:49 -0400

    Delete GameScripts directory

--- GameScripts/Loader.lua
+++
@@ -1,6 +0,0 @@
-local place_id = 7991339063 -- Replace with your actual Place ID
-
-if game.PlaceId == place_id then
-    local loadstring_script = loadstring(game:HttpGet("https://raw.githubusercontent.com/OfficialOasisScript/OasisGameScripts/main/MainScripts/RainbowFriends2.lua", true))
-    loadstring_script()
-end
